689aef94ac1e558902b0c105e51ea1f0b98b2f3f
Katoinga <evaldiviaci@unsa.edu.pe>
2020-12-08 14:32:51 -0500

    vrect

--- src/components/DashRectangle.js
+++ src/components/DashRectangle.js
@@ -4,10 +4,28 @@ import './styles/DashRectangle.css';
 class aDashRectangle extends React.Component {
   render(){
     return(
-      <div className="rectanglered">
+      <div className="rectangles">
+        <div className= "rectanglered">
         <h3>algo</h3>
         <h1>algo</h1>
+        </div>
+        <div className= "rectanglegreen">
+        <h3>algo</h3>
+        <h1>algo</h1>
+        </div>
+        <div className= "rectangleorange">
+        <h3>algo</h3>
+        <h1>algo</h1>
+        </div>
+        <div className= "rectanglecyan">
+        <h3>algo</h3>
+        <h1>algo</h1>
+        </div>
+
+
       </div>
+      
+      
     )
   }
 }
