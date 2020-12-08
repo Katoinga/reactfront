65db0b8ca86aab29028a98275b85fed476ab87aa
Katoinga <evaldiviaci@unsa.edu.pe>
2020-12-08 13:58:44 -0500

    v1.1

---
+++ src/components/aDashRectangle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/aDashRectangle.css';
+
+class aDashRectangle extends React.Component {
+  render(){
+    return(
+      <div className="rectangle3">
+        <h3>algo</h3>
+
+      </div>
+    )
+  }
+}
+export default aDashRectangle
