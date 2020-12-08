b3628f0aa232389f23c9f6290d4072ec06462f7d
SergioSoncco <axulsc@gmail.com>
2020-12-08 13:14:23 -0500

    component Login

---
+++ src/components/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './styles/Login.css';
+import logoImage from '../images/logo.png'
+
+class Login extends React.Component {
+  render(){
+    return(
+      <div className="contLogin">
+        <h3>SISTEMA DE ADMINISTRACIÓN</h3>
+        <h1>INICIA SESIÓN</h1>
+        <img src={logoImage} className="logoImage"></img>
+        <form className="loginForm">
+          <label>* DNI (8 caracteres)</label><br></br>
+          <input type="text"></input><br></br>
+          <label>* Contraseña</label><br></br>
+          <input type="password"></input><br></br>
+          <button className="loginButton">INGRESAR</button>
+        </form>
+      </div>
+    )
+  }
+}
+export default Login
