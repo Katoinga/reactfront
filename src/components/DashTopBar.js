4248186106a6f8490a8be8ed1b535f0068d2d81c
SergioSoncco <axulsc@gmail.com>
2020-12-11 22:48:35 -0500

    color to dashtopbar

--- src/components/DashTopBar.js
+++ src/components/DashTopBar.js
@@ -7,6 +7,7 @@ class DashTopBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      color : props.color,
       title : props.title,
       imagePerfil : props.imagePerfil,
       name : props.name,
@@ -20,7 +21,7 @@ class DashTopBar extends React.Component {
       <div className="navtop">
         <div className="header">
         </div>
-        <div className="selectbutton-purple">
+        <div className={this.state.color}>
         </div>
         <svg data-layer="ddcd63c5-a87b-4e65-8f9e-eccb39b07fff" preserveAspectRatio="none" viewBox="4.5 4.5 27 27" class="icon"><path d="M 4.5 19.5 L 16.5 19.5 L 16.5 4.5 L 4.5 4.5 L 4.5 19.5 Z M 4.5 31.5 L 16.5 31.5 L 16.5 22.5 L 4.5 22.5 L 4.5 31.5 Z M 19.5 31.5 L 31.5 31.5 L 31.5 16.5 L 19.5 16.5 L 19.5 31.5 Z M 19.5 4.5 L 19.5 13.5 L 31.5 13.5 L 31.5 4.5 L 19.5 4.5 Z"  /></svg>
         <p>{this.state.title}</p>
--- src/pages/AdminDashboardPage.js
+++ src/pages/AdminDashboardPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import DashSidebar from '../components/DashSidebar.js';
+import DashTopBar from '../components/DashTopBar.js';
 
 class AdminDashboardPage extends React.Component {
   render() {
     return (
       <div className="sidebar">
         <DashSidebar/>
+        <DashTopBar color="selectbutton-yellow"/>
       </div>
     )
   }
