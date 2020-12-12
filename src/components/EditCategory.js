a4b46db22e73b6232bd142c046af7572365177a9
SergioSoncco <axulsc@gmail.com>
2020-12-11 23:22:58 -0500

    EditCategory component

---
+++ src/components/EditCategory.js
@@ -0,0 +1,43 @@
+import React  from 'react';
+import './styles/NewCategory.css';
+
+class EditCategory extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      image : props.image,
+      name : props.name,
+      description : props.description
+    }
+  }
+  render() {
+    return (
+      <div className="containerNewCategory">
+        <p className="title">Categorias</p>
+        <p className="description">Completa el formulario con los datos solicitados</p>
+        <svg data-layer="74dcd9f3-4d01-47d8-a752-1e17bef85793" preserveAspectRatio="none" viewBox="-1.125 -1.125 482.25 2.25" class="divider"><path d="M 0 0 L 480 0"></path></svg>
+        <p className="imagetitle">Selecciona una foto para esta categoria</p>
+        <div className="containerImage">
+          <img className="image" src={this.state.image}>
+
+          </img>
+          <div className="selectImage">
+            <p className="imageDes">Suelta el archivo aqui</p>
+            <button className="seleccionar">Seleccionar</button>
+          </div>
+        </div>
+        <div className="form">
+          <label>Nombre de la categoria (20 caracteres max)</label><br></br>
+          <input type="text" value={this.props.name}></input><br></br>
+          <label>Descripción de la categoría (50 caracteres max)</label><br></br>
+          <input type="text" value={this.state.description}></input><br></br>
+        </div>
+        <button className="cancelar">Cancelar</button>
+        <button className="guardar">Guardar</button>
+      </div>
+    )
+  }
+}
+
+export default EditCategory
