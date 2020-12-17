51fa8df4ef83b2097ce31f9d49674fdee36f954d
Katoinga <evaldiviaci@unsa.edu.pe>
2020-12-17 00:15:03 -0500

    cam1

---
+++ src/pages/NewComprob.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Comanda from '../components/Comanda.js';
+import CajeroRecords from '../components/CajeroRecords.js';
+import CajeroTopBar from '../components/CajeroTopBar.js';
+import CajeroGenerarComprobantes from '../components/CajeroGenerarComprobantes.js';
+import NewComprobante from '../components/NewComprobante';
+class NewComprob extends React.Component {
+  render() {
+    return (
+      <div>
+        <NewComprobante/>
+      </div>
+    )
+  }
+}
+
+export default NewComprob
---
+++ src/pages/NewComprob2.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Comanda from '../components/Comanda.js';
+import CajeroRecords from '../components/CajeroRecords.js';
+import CajeroTopBar from '../components/CajeroTopBar.js';
+import CajeroGenerarComprobantes from '../components/CajeroGenerarComprobantes.js';
+import NewComprobante2 from '../components/NewComprobante2';
+class NewComprob2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <NewComprobante2/>
+      </div>
+    )
+  }
+}
+
+export default NewComprob2
