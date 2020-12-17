ae71ef1d85e0e7b855ff32b596bed9adcabf7d4c
Katoinga <evaldiviaci@unsa.edu.pe>
2020-12-17 00:13:47 -0500

    nc

---
+++ src/components/NewComprobante2.js
@@ -0,0 +1,42 @@
+import React  from 'react';
+import './styles/NewComprobante.css';
+import logoImage from '../images/logo.png'
+
+class NewUser extends React.Component {
+  render() {
+    return (
+      
+      <div className="adminPersonalNuevoUsuario">
+      <div  class="modal">            <svg preserveAspectRatio="none" viewBox="-0.75 -0.75 801.5 451.5" class="path8"><path d="M 0 0 L 800 0 L 800 450 L 0 450 L 0 0 Z"  /></svg>
+            <div class="modalHeader">                <div  class="personal">Comprobante</div>
+                <div class="completaElFormularioConLosDatosSolicitados">Completa el formulario con los datos solicitados</div>
+                <svg  preserveAspectRatio="none" viewBox="-1.125 -1.125 782.25 2.25" class="divider"><path d="M 0 0 L 779.9999389648438 0"  /></svg>
+</div>
+            
+            <div  class="xNombres50CaracteresMax"><span class="xNombres50CaracteresMax-0">*</span><span class="xNombres50CaracteresMax-1">Comprobante 5342</span></div>
+            <div class="xApellidoPaterno30CaracteresMax"><span class="xApellidoPaterno30CaracteresMax-0">*</span><span class="xApellidoPaterno30CaracteresMax-1"> Nombre Cajero Role</span></div>
+            <label type="text" id="namecl" name="namecl" class="rectangle58t" >DATA</label>
+            <div class="xApellidoMaterno30CaracteresMax"><span class="xApellidoMaterno30CaracteresMax-0">*</span><span class="xApellidoMaterno30CaracteresMax-1"> Nombre Cliente </span></div>
+            <label id="dni" name="dni" class="rectangle59t" >DATA2</label>
+            <div class="xDni8Caracteres"><span class="xDni8Caracteres-0">*</span><span class="xDni8Caracteres-1"> DNI cliente (8 caracteres)</span></div>
+            <label id="date" name="date" class="rectangle60t" >DATA3</label>
+            <div  class="xNroCelular9Caracteres"><span class="xNroCelular9Caracteres-0">*</span><span class="xNroCelular9Caracteres-1">Fecha</span></div>
+            <label id="address" name="address" class="rectangle63t" >DATA4</label>
+            <div  class="xDireccion70CaracteresMax"><span class="xDireccion70CaracteresMax-0">*</span><span class="xDireccion70CaracteresMax-1"> Dirección (70 caracteres máx..)</span></div>
+            <div  class="productos">Productos:</div>
+            <div  class="productos-arr">AQUI VA ARREGLO</div>
+
+            
+            
+            <div  class="total">Total S/.</div>
+            <div  class="total-dinero">3131</div>
+   
+            <button class="buttong">Cancelar</button>
+            <button class="buttonc">Salir</button>
+</div>
+      </div>
+    )
+  }
+}
+
+export default NewUser
